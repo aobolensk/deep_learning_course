76d486a0b76ccef8560ad48bf0744f52c8dd89b8
Arseniy Obolenskiy <gooddoog@student.su>
2022-04-19 21:22:59 +0300

    Remove unnecessary coco dataset download

--- workdir/bootstrap.sh
+++ workdir/bootstrap.sh
@@ -18,16 +18,6 @@ cd pytorch_wavelets
 python3 -m pip install .
 cd ..
 
-cd yolor/
-mkdir -p coco/images
-cd coco/images
-wget http://images.cocodataset.org/zips/val2017.zip
-unzip val2017.zip > /dev/null
-rm val2017.zip
-cd ../../..
-
-cd yolor
-python3 -m pip install --upgrade --no-cache-dir gdown
 gdown 1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76
 
 # TEST
